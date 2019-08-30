79a9f3107f428c1b385d62efecd2bd40de867cc5
huangqiao <huangqiao92@gmail.com>
2019-08-30 14:52:53 +0800

    加入编辑本机号码功能。

---
+++ app/src/main/java/com/szhr/contacts/AddContactActivity.java
@@ -0,0 +1,22 @@
+package com.szhr.contacts;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddContactActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_add_contact;
+    }
+
+    @Override
+    protected void setup(View contentView) {
+
+    }
+}
