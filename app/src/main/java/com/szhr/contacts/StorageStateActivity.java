ad2a397a6f094b004993b5813144e10306f620a5
huangqiao <huangqiao92@gmail.com>
2019-09-04 12:59:35 +0800

    加入存储器状态界面。

---
+++ app/src/main/java/com/szhr/contacts/StorageStateActivity.java
@@ -0,0 +1,176 @@
+package com.szhr.contacts;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.RemoteException;
+import android.os.StatFs;
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.internal.telephony.AdnRecord;
+import com.android.internal.telephony.IIccPhoneBook;
+import com.android.internal.telephony.IccConstants;
+import com.android.os.ServiceManager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class StorageStateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle("存储器状态");
+    }
+
+    @Override
+    protected void setupBottom(TextView leftTv, ImageView rightIv) {
+        leftTv.setText("");
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_storage_state;
+    }
+
+    @Override
+    protected void setup(View contentView) {
+        TextView simCapacityTv = contentView.findViewById(R.id.simCapacityTv);
+        TextView simUsedTv = contentView.findViewById(R.id.simUsedTv);
+        TextView simFreeTv = contentView.findViewById(R.id.simFreeTv);
+        TextView phoneCapacityTv = contentView.findViewById(R.id.phoneCapacityTv);
+        TextView phoneUsedTv = contentView.findViewById(R.id.phoneUsedTv);
+        TextView phoneFreeTv = contentView.findViewById(R.id.phoneFreeTv);
+
+        // TODO Uncomment following code.
+//        try {
+//            int gsmPhonebookCapacity = ICCPhonebook.getGsmPhonebookCapacity();
+//            int gsmPhonebookSize = ICCPhonebook.getGsmPhonebookSize();
+//            simCapacityTv.setText(String.format(Locale.CHINA, "手机总空间：%d 条", gsmPhonebookCapacity));
+//            simFreeTv.setText(String.format(Locale.CHINA, "剩余空间：%d 条", gsmPhonebookCapacity - gsmPhonebookSize));
+//            simUsedTv.setText(String.format(Locale.CHINA, "已用空间：%d 条", gsmPhonebookSize));
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
+
+        long totalInternalMemorySize = getTotalInternalMemorySize();
+        long availableInternalMemorySize = getAvailableInternalMemorySize();
+
+        phoneCapacityTv.setText(String.format(Locale.CHINA, "手机总空间：%s",
+                Formatter.formatFileSize(this, totalInternalMemorySize)));
+        phoneFreeTv.setText(String.format(Locale.CHINA, "剩余空间：%s",
+                Formatter.formatFileSize(this, availableInternalMemorySize)));
+        phoneUsedTv.setText(String.format(Locale.CHINA, "已用空间：%s",
+                Formatter.formatFileSize(this, totalInternalMemorySize - availableInternalMemorySize)));
+
+    }
+
+    /**
+     * 获取手机内部空间总大小
+     *
+     * @return 大小，字节为单位
+     */
+    private long getTotalInternalMemorySize() {
+        //获取内部存储根目录
+        File path = Environment.getDataDirectory();
+        //系统的空间描述类
+        StatFs stat = new StatFs(path.getPath());
+        //每个区块占字节数
+        long blockSize = stat.getBlockSizeLong();
+        //区块总数
+        long totalBlocks = stat.getBlockCountLong();
+        return totalBlocks * blockSize;
+    }
+
+    /**
+     * 获取手机内部可用空间大小
+     *
+     * @return 大小，字节为单位
+     */
+    private long getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        //获取可用区块数量
+        long availableBlocks = stat.getAvailableBlocksLong();
+        return availableBlocks * blockSize;
+    }
+
+    /**
+     * The CDMA card is unaccessible when in fly-mode， consequently, please
+     * ensure isFlyMode() is true before call other methods;
+     *
+     * @author chencc
+     */
+    public final static class ICCPhonebook {
+        private static final String gsmPhonebook = "simphonebook";
+        private static final String cdmaPhonebook = "simphonebook3";
+
+        public static boolean isFlyMode() {
+            // TODO
+            return false;
+        }
+
+        /**
+         * @return 有效的联系人记录个数
+         * @throws RemoteException
+         */
+        public static int getGsmPhonebookSize() throws RemoteException {
+            return getPhonebookSize(gsmPhonebook);
+        }
+
+        public static int getCdmaPhonebookSize() throws RemoteException {
+            return getPhonebookSize(cdmaPhonebook);
+        }
+
+        public static int getGsmPhonebookCapacity() throws RemoteException {
+            int size[] = getIccPhonebook(gsmPhonebook).getAdnRecordsSize(
+                    IccConstants.EF_ADN);
+            return size[2];
+        }
+
+        public static int getCdmaPhonebookCapacity() throws RemoteException {
+            return getAdnRecords(getIccPhonebook(cdmaPhonebook)).size();
+        }
+
+        private static int getPhonebookSize(String service)
+                throws RemoteException {
+            List<AdnRecord> adnRecordList = getAdnRecords(getIccPhonebook(service));
+
+            int i = 0;
+            for (AdnRecord r : adnRecordList) {
+                if (!r.isEmpty()) {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+
+        private static List<AdnRecord> getAdnRecords(IIccPhoneBook simPhoneBook)
+                throws RemoteException {
+            List<AdnRecord> adnRecordList = simPhoneBook
+                    .getAdnRecordsInEf(IccConstants.EF_ADN);
+            // do it twice cause the second time shall read from cache only
+            adnRecordList = simPhoneBook.getAdnRecordsInEf(IccConstants.EF_ADN);
+            if (adnRecordList == null) {
+                adnRecordList = new ArrayList<AdnRecord>();
+            }
+            return adnRecordList;
+        }
+
+        public static IIccPhoneBook getIccPhonebook(String service) {
+            IIccPhoneBook simPhoneBook = IIccPhoneBook.Stub
+                    .asInterface(ServiceManager.getService(service));
+            if (simPhoneBook == null)
+                throw new NullPointerException("Can't get the Service: "
+                        + service);
+            return simPhoneBook;
+        }
+    }
+}
